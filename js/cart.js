d381758bbde15b9d872c03237933934181f0010d
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-11-27 16:38:07 +0300

    day1

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+const cart = () => {
+  const cartBtn = document.querySelector(".button-cart");
+  const cart = document.getElementById("modal-cart");
+  const closeBtn = cart.querySelector(".modal-close");
+
+  console.log(cart);
+  cartBtn.addEventListener("click", () => {
+    cart.style.display = "flex";
+  });
+  closeBtn.addEventListener("click", () => {
+    cart.style.display = "";
+  });
+};
+cart();
---
+++ js/search.js
@@ -0,0 +1,12 @@
+const search = () => {
+  const input = document.querySelector(".search-block > input");
+  const searchBtn = document.querySelector(".search-block > button");
+  console.log('input: ', input);
+  console.log('searchBtn: ', searchBtn);
+
+  input.addEventListener('input', (event) => {
+    console.log(event.target.value);
+  })
+};
+
+search();
