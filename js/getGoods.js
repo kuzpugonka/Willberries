47133e47cc00347ad341dc625a8b28559f358a76
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-11-29 14:38:44 +0300

    фильтрация по категориям

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,22 +1,30 @@
 const getGoods = () => {
-  const links = document.querySelectorAll(".navigation-link"); //сначала получаем переменную
-  // описываем функции
-  const getData = () => {
+  const links = document.querySelectorAll(".navigation-link"); //сначала получаем все ссылки из меню
+
+  const getData = (value, category) => {
     fetch("/db/db.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log("data: ", data);
-        localStorage.setItem('data', JSON.stringify(data))
+        // console.log("data: ", data);
+
+        const array = category
+          ? data.filter((item) => item[category] === value)
+          : data;
+
+        localStorage.setItem("goods", JSON.stringify(array));
       });
   };
   // используем функции
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
-      e.preventDefault();
-      getData();
+      e.preventDefault(); // убираем стандартное поведение ссылки
+      const linkValue = link.textContent;
+      // console.log('linkValue: ', linkValue);
+      const category = link.dataset.field;
+      // console.log('category: ', category);
+      getData(linkValue, category);
     });
   });
-  
 };
 
 getGoods();
