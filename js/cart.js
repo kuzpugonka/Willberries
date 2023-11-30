ea51c5c70452709237cd19d98b7769479c0829e9
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-12-01 00:04:12 +0300

    изм кол товаров в корзине день4

--- js/cart.js
+++ js/cart.js
@@ -5,6 +5,41 @@ const cart = () => {
   const goodsContainer = document.querySelector(".long-goods-list");
   const cartTable = document.querySelector(".cart-table__goods");
 
+  const deleteCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const newCart = cart.filter((good) => good.id !== id);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+
+  const plusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+
+    const newCart = cart.map((good) => {
+      if (good.id === id) {
+        good.count++;
+      }
+      return good;
+    });
+
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+
+  const minusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const newCart = cart.map((good) => {
+      if (good.id === id) {
+        if (good.count > 1) {
+          good.count--;
+        }
+      }
+      return good;
+    });
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+
   const addToCart = (id) => {
     const goods = JSON.parse(localStorage.getItem("goods"));
     const clickedGood = goods.find((good) => good.id === id);
@@ -28,6 +63,7 @@ const cart = () => {
   };
 
   const renderCartGoods = (goods) => {
+    cartTable.innerHTML = "";
     goods.forEach((good) => {
       const tr = document.createElement("tr");
       tr.innerHTML = `
@@ -39,7 +75,18 @@ const cart = () => {
         <td>${+good.price * +good.count}$</td>
         <td><button class="cart-btn-delete">x</button></td>
       `;
+
       cartTable.append(tr);
+
+      tr.addEventListener("click", (event) => {
+        if (event.target.classList.contains("cart-btn-minus")) {
+          minusCartItem(good.id);
+        } else if (event.target.classList.contains("cart-btn-plus")) {
+          plusCartItem(good.id);
+        } else if (event.target.classList.contains("cart-btn-delete")) {
+          deleteCartItem(good.id);
+        }
+      });
     });
   };
 
@@ -51,8 +98,24 @@ const cart = () => {
     renderCartGoods(cartArray);
     cart.style.display = "flex";
   });
+
   closeBtn.addEventListener("click", () => {
     cart.style.display = "";
+  });
+
+  cart.addEventListener("click", (event) => {
+    if (
+      !event.target.closest(".modal") &&
+      event.target.classList.contains("overlay")
+    ) {
+      cart.style.display = "";
+    }
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      cart.style.display = "";
+    }
   });
 
   if (goodsContainer) {
